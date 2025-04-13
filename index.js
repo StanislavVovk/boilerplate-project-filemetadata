158a280ca4674f237ef0889fad00cb20b75b96d9
weeweeslav <stassiuris@gmail.com>
2025-04-13 23:39:21 +0300

    solved

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require("multer");
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+function uploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  const file = req.files[0]
+  res.json({ name: file.originalname, type: file.mimetype, size: file.size });
+}
+
+
+app.post('/api/fileanalyse', upload.any("upfile"), uploadFiles);
 
 
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
